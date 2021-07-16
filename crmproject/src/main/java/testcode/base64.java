e4229bcba82f1b135e7d4922b91426ea257c47aa
Jenzert1609 <83209629+Jenzert1609@users.noreply.github.com>
2021-07-16 15:26:17 +0700

    Create base64.java

---
+++ crmproject/src/main/java/testcode/base64.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testcode;
+
+/**
+ *
+ * @author thanh
+ */
+public class base64 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
