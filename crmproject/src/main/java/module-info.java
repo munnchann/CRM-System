198a4a6234c5d442e1cb49551a94cf55b6ded314
Nguyễn Ngọc Tú Quyên <quyennguyen.qn587@gmail.com>
2021-07-16 14:54:56 +0700

    submit all login view
    new form login

--- crmproject/src/main/java/module-info.java
+++ crmproject/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.quyen.crmproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.base;
     
     opens com.quyen.crmproject to javafx.fxml;
     exports com.quyen.crmproject;
